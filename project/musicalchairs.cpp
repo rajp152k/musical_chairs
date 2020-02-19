a8b3291852e46b1f96d1a4261331030bd1643985
Raj Patil <rajp152000@gmail.com>
2020-02-19 19:07:38 +0530

    refactored working tree

--- project/musicalchairs.cpp
+++ project/musicalchairs.cpp
@@ -11,8 +11,14 @@
 #include <getopt.h>  /* for getopt */
 #include <assert.h>  /* for assert */
 #include <chrono>	/* for timers */
-
-
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <vector>
+#include <cstdlib>
+#include <random>
+#include <algorithm>
+using namespace std;
 /*
  * Forward declarations
  */
@@ -93,36 +99,131 @@ void usage(int argc, char *argv[])
     exit(EXIT_FAILURE);
 }
 
-//void umpire_main(int nplayers)
-//{
-//    /* Add your code here */
-//	/* read stdin only from umpire */
-//	return;
-//}
-//
-//void player_main(int plid)
-//{
-//    /* Add your code here */
-//	/* synchronize stdouts coming from multiple players */
-//	return;
-//}
+
+//custom function declarations; definitions in the end
+
+int random(int);
+void setup(int);
+void choose(int);
+
+struct Pinfo{
+	//creating an array in heap that can be read by everyone
+	int id;
+	bool alive;
+	bool sitting;
+	int position;
+	int velocity;
+	int sleep_time;//set before every turn: in microseconds
+};
+
+struct Shared{//storage of common shared variables
+	int NP;
+	thread* Players;
+	Pinfo* player_info;
+	int chairs;
+	int* chair_status;
+	int standing_count;
+};
+
+
+struct Shared shared;
+
+void umpire_main(int nplayers)
+{
+	printf("umpire created\n");
+
+	return;
+}
+
+void player_main(int plid)
+{
+	printf("players created: id :: %d\n",plid);
+	/* synchronize stdouts coming from multiple players */
+	return;
+}
 
 
 //all the relevant code is roots from musical_chairs
 unsigned long long musical_chairs(int nplayers)
 {
+	srand(time(0));
 	auto t1 = chrono::steady_clock::now();
 
-	// Spawn umpire thread.
-    /* Add your code here */
+	thread umpire(umpire_main,nplayers);
 
-	// Spawn n player threads.
-    /* Add your code here */
+	shared.player_info = new Pinfo[nplayers];
+	shared.Players = new thread[nplayers];
+	shared.chair_status = new int[nplayers-1];
 
+	//first setup: creating the players
+	for(auto i=0;i<nplayers;i++){
+		shared.Players[i] = thread(player_main,i);
+	}
+
+
+	//waiting for players to join
+	for(auto i=0;i<nplayers;i++){
+		shared.Players[i].join();
+	}
+	//waiting for umpire to join
+	umpire.join();
 	auto t2 = chrono::steady_clock::now();
-
 	auto d1 = chrono::duration_cast<chrono::microseconds>(t2 - t1);
+
+	delete []  shared.Players;
+	delete []  shared.player_info;
+	delete []  shared.chair_status;
 
 	return d1.count();
 }
 
+void setup(int n){
+	//given the condition of n players and n-1 chairs
+	//this randomly assigns the positions to the players
+	//has global side-effects
+	for(auto i=0;i<n;i++){
+		shared.Players[i] = thread(player_main,i);
+		shared.player_info[i].alive=true;
+		shared.player_info[i].sitting=false;
+	}
+	//FUNCTION CALL TO SEAT ARRANGER
+}
+
+int random(int n){
+	return rand()%n;
+}
+
+void step(int i){//called on shared.player_info[i]
+	//called as per lock step synchronization
+	if(shared.player_info[i].position == shared.chairs-1 &&
+	   shared.player_info[i].velocity == 1){
+		shared.player_info[i].velocity=-1;
+	}
+	else if(shared.player_info[i].position == 0 &&
+		shared.player_info[i].velocity == -1 ){
+		shared.player_info[i].velocity=1;
+	}
+	else{
+		shared.player_info[i].position +=
+		shared.player_info[i].velocity;
+	}
+}
+
+void choose(int i){//called on shared.player_info[i]
+	//called as per lock step synchronization
+	if(shared.chair_status[shared.player_info[i].position] ==-1 &&
+	   shared.player_info[i].alive){
+		if(shared.player_info[i].position % 2 ==1 &&
+		   shared.player_info[i].velocity == 1){
+			shared.chair_status[shared.player_info[i].position] = i;
+			shared.player_info[i].sitting=true;
+			shared.standing_count--;
+		}
+		if(shared.player_info[i].position % 2 ==0 &&
+		   shared.player_info[i].velocity == -1){
+			shared.chair_status[shared.player_info[i].position] = i;
+			shared.player_info[i].sitting=true;
+			shared.standing_count--;
+		}
+	}
+}
